2c30bdfb1de62e0c17b0e9c40e886f91c326ecb4
Ahsan Imran <111527387+ahsanimran456@users.noreply.github.com>
2023-06-03 22:29:05 +0500

    Add files via upload

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
+import Slider from '../../Components/Slider/Slider'
 export default function Home() {
   return (
     <>
     <Header/>
+    <Slider/>
     </>
   )
 }
