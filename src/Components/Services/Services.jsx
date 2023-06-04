ebebbe2ee6c60ecd1dfc3590b9829af06633633b
ahsanimran54080 <111527387+ahsanimran456@users.noreply.github.com>
2023-06-05 00:56:26 +0500

    update

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,9 +1,33 @@
 function Services() {
-    return ( 
+    return (
         <section className="Our-Services">
-            
+            <div className="container">
+                <div className="text-center">
+                    <div className="mx-auto about-show mb-3"></div>
+                    <h1 className="text-dark">
+                        Our Services
+                    </h1>
+                </div>
+                <div className="row service-cards">
+                    <div className="col-md-4">
+                        <div className="service-each-card">
+                            <div >
+                                <img src="https://demo.htmlcodex.com/2565/cctv-camera-website-template/img/icon/icon-6.png" alt="" />
+                            </div>
+                            <div>
+                                <h4>
+                                    Commercial CCTV System
+                                </h4>
+                            </div>
+                            <div>
+                                <p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
-     );
+    );
 }
 
 export default Services;
